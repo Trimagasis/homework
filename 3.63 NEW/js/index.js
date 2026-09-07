f9fa7806641d98bd2f6201ebb24d47a4709a2d1d
Trimagasis <77401414+Trimagasis@users.noreply.github.com>
2026-09-07 16:36:17 +0300

    Add files via upload

---
+++ 3.63 NEW/js/index.js
@@ -0,0 +1,52 @@
+"use strict";
+
+// {
+//     id: 1,
+//     text: "Todo",
+//     is_completed: false,
+// }
+
+const todos = [];
+
+const todoKeys = {
+	id: "id",
+	text: "text",
+	is_completed: "is_completed",
+};
+
+const getNewTodoId = (todos) =>
+	todos.reduce((maxId, todo) => Math.max(maxId, todo[todoKeys.id]), 0) + 1;
+
+const createTodo = (todos, text) => {
+	const newTodo = {
+		[todoKeys.id]: getNewTodoId(todos),
+		[todoKeys.text]: text,
+		[todoKeys.is_completed]: false,
+	};
+	todos.push(newTodo);
+	return newTodo;
+};
+
+const completeTodoById = (todos, todoId) => {
+	const todo = todos.find((todo) => todo[todoKeys.id] === todoId);
+	if (todo == undefined) {
+		console.error(`Todo with id ${todoId} not found`);
+		return null;
+	}
+	todo[todoKeys.is_completed] = !todo[todoKeys.is_completed];
+	return todo;
+};
+
+const deleteTodoById = (todos, todoId) => {
+	const todoIndex = todos.findIndex((todo) => todo[todoKeys.id] === todoId);
+	if (todoIndex === -1) {
+		console.error(`Todo with id ${todoId} not found`);
+		return todos;
+	}
+	todos.splice(todoIndex, 1);
+	return todos;
+};
+
+// const deleteTodoById = (todos, todoId) => {
+// 	return todos.filter((todo) => todo[todoKeys.id] != todoId);
+// };
